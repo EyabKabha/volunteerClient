a0b706ee657cf90a4085763cc14eea3bb61b15c9
EyabKabha <38509837+EyabKabha@users.noreply.github.com>
2020-05-21 10:39:00 +0300

    fixing conflict and adding some files

--- src/HomePage1.js
+++ src/HomePage1.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Redirect, withRouter } from 'react-router-dom';
 import Card from './ExplainCard';
 import { Button, Navbar, Nav, NavDropdown, Form, FormControl } from 'react-bootstrap';
+import Footer from './Footer';
 import './Styling/Home.css';
 
 export default class Homepage extends React.Component {
@@ -9,30 +10,34 @@ export default class Homepage extends React.Component {
         super(props);
         this.state = {
             btnEvent: false,
+            btnNewEvent: false,
         }
     }
     onClickBtn = () => {
         this.setState({ btnEvent: true })
         console.log(this.state.btnEvent)
     }
+    onClickEvent = () => {
+        this.setState({ btnNewEvent: true })
+    }
     render() {
         return (
 
-            <div className="container-fluid">
+            <div className="container-fluid" id="container">
                 <div className="row">
-                    <div id="mainNav" className="col p-0">
+                    <div id="mainNav" className="col">
                         <Navbar bg="dark" variant="dark" sticky="top">
                             <Navbar.Brand href="#home">Voulnteer</Navbar.Brand>
                             <Nav className="mr-auto">
                                 <Nav.Link href="#home">Home</Nav.Link>
-                                <Nav.Link href="#features">Features</Nav.Link>
-                                <Nav.Link href="#pricing">Help</Nav.Link>
+                                <Nav.Link href="#features">Dashboard</Nav.Link>
+                                <Nav.Link href="#pricing">My events</Nav.Link>
                             </Nav>
                             <Form inline>
                                 {/* <FormControl type="text" placeholder="Search" className="mr-sm-2" /> */}
-                                <Button variant="outline-info">Log In</Button>&nbsp;&nbsp;
+                                <Button href="/login" variant="outline-info">Log In</Button>&nbsp;&nbsp;
                                 <Navbar.Text> <small>or</small></Navbar.Text>
-                                <Nav.Link href="#deets">SignUp</Nav.Link>
+                                <Nav.Link href="/signup/">SignUp</Nav.Link>
                             </Form>
                         </Navbar>
                     </div>
@@ -42,11 +47,14 @@ export default class Homepage extends React.Component {
                         <button type="button" className="btn btn-primary btn-block mb-2" id="Alleventsbtn" onClick={this.onClickBtn}>All Events</button>
                     </div>
                     <div className="col-6">
-                        <button type="button" className="btn btn-success btn-block mb-2" id="Createeventbtn">Create Event</button>
+                        <button type="button" className="btn btn-success btn-block mb-2" id="Createeventbtn" onClick={this.onClickEvent}>Create Event</button>
                     </div>
                 </div>
-                <Card/>
+                <Card header="All Events" desc="See all volunteering events happenning near you." dir="rtl" src='/event.jpg' />
+                <Card header="Create Event" desc="Create a volunteering & invite people to join." dir="ltr" src='/createevent.png' />
+                <Footer />
                 {this.state.btnEvent && <Redirect to='/event/available' />}
+                {this.state.btnNewEvent && <Redirect to='/create/event/' />}
             </div>
 
         )
